a45543e13eee4af5e5072dea2ec2992df9849af9
Joe Ratzer <joe.ratzer@msn.com>
2016-11-20 21:44:27 +0000

    Update keep-db-awake-scheduled-task.ps1

--- keep-db-awake-scheduled-task.ps1
+++ keep-db-awake-scheduled-task.ps1
@@ -4,7 +4,7 @@ $taskName = "KeepSqlExpressAwake"
 
 #action and trigger
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument $psFileLocationAndName
-$trigger = New-ScheduledTaskTrigger -Once -At 7am -RepetitionDuration  (New-TimeSpan -Days (365*20))  -RepetitionInterval  (New-TimeSpan -Minutes 5)	
+$trigger = New-ScheduledTaskTrigger -Once -At 7am -RepetitionDuration  (New-TimeSpan -Days (365*20))  -RepetitionInterval  (New-TimeSpan -Minutes 3)	
 
 #create scheduled task
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description "Process to prevent SQL Express sleeping"
